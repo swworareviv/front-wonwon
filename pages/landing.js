1d572e36aa06a5f6a8c9e4c1b1e3bc1900234717
pomlim <passavich.lim@gmail.com>
2023-08-01 00:46:14 +0900

    add banner in landing page

--- pages/landing.js
+++ pages/landing.js
@@ -6,21 +6,25 @@ import wonwon_icon from '@/public/wonwon_icon.svg';
 
 const LandingPage = ({}) => {
   const url = `/shops`;
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+    if (newWindow) newWindow.opener = null;
+  };
   return (
-    <div className="w-full p-4 bg-butter-default ">
+    <div className="w-full h-fit bg-butter-default ">
       <div className="text-center">
         <Image
           alt="logo"
           src={wonwon_icon}
           width={200}
-          height={150}
+          height={120}
           style={{
             filter:
               'brightness(0) saturate(100%) invert(78%) sepia(11%) saturate(4156%) hue-rotate(13deg) brightness(105%) contrast(72%)'
           }}
         />
         <div>
-          <Image alt="description" src={Landing_img} width={310} height={310} />
+          <Image alt="description" src={Landing_img} width={210} height={210} />
         </div>
         <div className="flex flex-col items-center justify-center mt-4 text-xs font-normal text-brown-default font-kanit">
           <p className="text-xl font-medium text-center ">WonWon</p>
@@ -34,7 +38,7 @@ const LandingPage = ({}) => {
             <li className=" w-fit">รีวิวร้านให้ทุกคนรู้จัก</li>
           </ul>
         </div>
-        <div className="flex justify-center w-full pt-10 pb-8">
+        <div className="flex justify-center w-full pt-3 pb-1">
           <a href={url}>
             <button
               type="button"
@@ -43,6 +47,18 @@ const LandingPage = ({}) => {
               เริ่มใช้งาน
             </button>
           </a>
+        </div>
+        <div className="flex p-2 space-x-2 text-butter-light font-kanit">
+          <div
+            onClick={() =>
+              openInNewTab(
+                'https://www.google.com'
+              )
+            }
+            className=" cursor-pointer bg-[#835020] h-[48px] rounded-lg w-full justify-center items-center flex "
+          >
+            <p>ติดตามฟีเจอร์ใหม่</p>
+          </div>
         </div>
       </div>
     </div>

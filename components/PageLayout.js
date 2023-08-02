a0d1ad5dffdd8c022eba3217e9af4dc598df0ef1
Meawhung <meawhung@gmail.com>
2023-08-02 23:12:41 +0900

    update meta

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,8 +1,28 @@
 import Layout from '@/components/common/Layout';
 import Header from '@/components/Header';
+import Head from 'next/head';
 
 const PageLayout = ({ children }) => {
-  return <Layout header={<Header />}>{children}</Layout>;
+  return (
+    <div>
+      <Head>
+        <title>WonWon by Reviv</title>
+        <meta
+          name="description"
+          content="Repair-Lifestyle platform in Thailand"
+          key="desc"
+        />
+        <meta property="og:title" content="WonWon by Reviv" />
+        <meta
+          property="og:description"
+          content="The 1st Repair-Lifestyle platform in Thailand that connects consumers with local repair shops.”e"
+        />
+        <meta property="og:image" content="/OG.png" />
+        <link rel="shortcut icon" href="/favicon.jpg" />
+      </Head>
+      <Layout header={<Header />}>{children}</Layout>
+    </div>
+  );
 };
 
 export default PageLayout;
